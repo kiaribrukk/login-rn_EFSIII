e5cadfcaec5f9233cef6bf6000790f080fe3107d
KIARA BRUKIEW <49518961@ort>
2026-06-09 12:01:55 -0300

    Siguiendo con el tp

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Usuario o contraseña incorrectos, vuelva a intentar</Text>
+      <Text>Usuario o contraseña incorrectos, vuelva a intentar nuevamente</Text>
       <StatusBar style="auto" />
     </View>
   );
